1f8a109ab3be5d0e3dc73a8d30ea0165e108f159
shibani-r <shibani.v94@gmail.com>
2016-09-29 14:04:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,13 @@ console.log('Loaded!');
 var element = document.getElementById("main-text");
 element.innerHTML = "new value";
 var img = document.getElementById("pic");
+var marginLeft = 0;
+function moveRight(){
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft +"px";
+    
+}
 img.onclick = function (){
+    var interval = setInterval(moveRight,100);
     img.style.marginLeft = "100px";
 };
