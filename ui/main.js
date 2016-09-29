be8703e7b5ddfe4ba2e76aaaedec146484397cae
shibani-r <shibani.v94@gmail.com>
2016-09-29 14:05:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,6 +9,5 @@ function moveRight(){
     
 }
 img.onclick = function (){
-    var interval = setInterval(moveRight,100);
-    img.style.marginLeft = "100px";
+    var interval = setInterval(moveRight,50);
 };
