210e5eb01f7169d2f173737ca6042414c6800629
shibani-r <shibani.v94@gmail.com>
2016-09-29 15:05:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,16 @@
 var button = document.getElementById("counter");
-var counter = 0;
 button.onclick = function(){
-    counter = counter + 1;
-    var span = document.getElementById("count");
+    var request = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function(){
+    // process the server response
+    if(request.readystate === XMLHttpRequest.DONE){
+        if(request.status === 200){
+        var counter = request.responseText;
+        var span = document.getElementById("count");
     span.innerHTML = counter.toString();
+    }
+    }
+};
+    request.open("GET","https://http://shibani-r.imad.hasura-app.io/",true);
+    request.send(null);
 }
