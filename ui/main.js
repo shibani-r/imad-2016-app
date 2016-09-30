f4fe5263d696fda9dcf130ce49f2e91ab9fdf477
shibani-r <shibani.v94@gmail.com>
2016-09-30 15:49:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,14 +23,15 @@ button.onclick = function (){
 //make the request
     request.open('GET','http://shibani-r.imad.hasura-app.io/counter', true);
     request.send(null);
-}
+};
 
 //submit name
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
-submit.onclick = function(){
+submit.onclick = function () {
     //make a request to the server and send the name
+    
     //capture a list of names and render it as a list
     var names = ['name1', 'name2', 'name3', 'name4'];
     var list = '';
@@ -39,5 +40,4 @@ submit.onclick = function(){
     }
     var ul = document.getElementById('namelist');
     ul.innerHTML = list;
-    }
 };
