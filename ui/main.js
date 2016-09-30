a38a4c086f38e2bc055863d229f23ba037b26017
shibani-r <shibani.v94@gmail.com>
2016-09-30 15:32:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,43 @@
+//counter code
 var button = document.getElementById('counter');
+
 button.onclick = function (){
+    
+    //create a request object
     var request = new XMLHttpRequest();
+    
+    //capture the response and store it in a variable
     request.onreadystatechange = function(){
     // process the server response
     if(request.readyState === XMLHttpRequest.DONE){
+        //take some action
         if(request.status === 200){
         var counter = request.responseText;
         var span = document.getElementById('count');
     span.innerHTML = counter.toString();
     }
     }
+    //not done yet
 };
+
+//make the request
     request.open('GET','http://shibani-r.imad.hasura-app.io/counter', true);
     request.send(null);
 }
+
+//submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementByID('submit_btn');
+submit.onclick = function(){
+    //make a request to the server and send the name
+    //capture a list of names and render it as a list
+    var names = ['name1', 'name2', 'name3', 'name4'];
+    var list = '';
+    for(var i=0; i<names.length; i++){
+        list += '<li> + names[i] + </li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+    }
+};
