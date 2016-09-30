399a68a59211af0b95acf31117a09a14c3b28b65
shibani-r <shibani.v94@gmail.com>
2016-09-30 16:06:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -103,9 +103,9 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 var names = [];
-app.get('/submit-name/:name', function(req,res){
+app.get('/submit-name', function(req,res){// /submit-nmae?name-xxxx
     //get the name from the request
-    var name = req.params.name;
+    var name = req.query.name;
     
     names.push(name);
     //JSON: javascript object notation
