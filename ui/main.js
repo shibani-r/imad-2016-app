b0845bc9423708b539f2be908d68e6e690f71818
shibani-r <shibani.v94@gmail.com>
2016-09-30 16:35:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,8 +26,7 @@ button.onclick = function (){
 };
 
 //submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
     //make a request to the server and send the name
@@ -56,6 +55,8 @@ submit.onclick = function () {
 };
 
 //make the request
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
     request.open('GET','http://shibani-r.imad.hasura-app.io/submit-name?name=' + name, true);
     request.send(null);
 };
