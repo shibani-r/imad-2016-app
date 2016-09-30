d03e2827f0948353a93dbea53f81cdb5265960a1
shibani-r <shibani.v94@gmail.com>
2016-09-30 16:13:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ app.get('/counter',function(req, res){
     res.send(counter.toString());
 })
 var names = [];
-app.get('/submit-name', function(req,res){// /submit-name?name-xxxx
+app.get('/submit-name', function(req,res){// /submit-name?name=xxxx
     //get the name from the request
     var name = req.query.name;
     
