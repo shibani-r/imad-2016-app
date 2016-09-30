8f8e43b2b18f1d3160a718c6ee6e0c1f36da9ea0
shibani-r <shibani.v94@gmail.com>
2016-09-30 16:07:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,6 +87,15 @@ app.get('/counter',function(req, res){
     counter = counter+1;
     res.send(counter.toString());
 })
+var names = [];
+app.get('/submit-name', function(req,res){// /submit-nmae?name-xxxx
+    //get the name from the request
+    var name = req.query.name;
+    
+    names.push(name);
+    //JSON: javascript object notation
+    res.send(JSON.stringify(names));
+})
 app.get('/:articleName', function (req, res){
     var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
@@ -102,15 +111,7 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-var names = [];
-app.get('/submit-name', function(req,res){// /submit-nmae?name-xxxx
-    //get the name from the request
-    var name = req.query.name;
-    
-    names.push(name);
-    //JSON: javascript object notation
-    res.send(JSON.stringify(names));
-})
+
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
