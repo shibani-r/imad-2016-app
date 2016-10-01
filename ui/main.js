8752b3902941e5904408d678d3afa4644bb5ab05
shibani-r <shibani.v94@gmail.com>
2016-10-01 15:39:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -61,3 +61,38 @@ var name = nameInput.value;
     request.send(null);
 };
    
+   //submit comment
+
+var submit = document.getElementById('add_btn');
+submit.onclick = function () {
+    //make a request to the server and send the comment
+    
+     //create a request object
+    var request = new XMLHttpRequest();
+    
+    //capture the response and store it in a variable
+    request.onreadystatechange = function(){
+    // process the server response
+    if(request.readyState === XMLHttpRequest.DONE){
+        //take some action
+        if(request.status === 200){
+         //capture a list of comments and render it as a list
+    var comments = request.responseText;
+    comments = JSON.parse(comments);
+    var clist = '';
+    for(var i=0; i< comments.length; i++){
+        clist += '<li>' + comments[i] + '</li>';
+    }
+    var ul = document.getElementById('commentlist');
+    ul.innerHTML = clist;
+    }
+    }
+    //not done yet
+};
+
+//make the request
+var commentInput = document.getElementById('comment');
+var comment = commentInput.value;
+    request.open('GET','http://shibani-r.imad.hasura-app.io/add-comment?comment=' + comment, true);
+    request.send(null);
+};
