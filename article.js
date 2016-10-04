edf462a0b234f24d256e647ea0a20c22f8f88a1d
shibani-r <shibani.v94@gmail.com>
2016-10-04 15:12:53 +0530

    [imad-console] Updates article.js

--- article.js
+++ article.js
@@ -1,3 +1,10 @@
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
+
+var app = express();
+app.use(morgan('combined'));
+
 //submit comment
 
 var add = document.getElementById('add_btn');
@@ -34,4 +41,9 @@ var comment = commentInput.value;
     request.send(null);
 
 };
+
+var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+app.listen(8080, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
+});
 
