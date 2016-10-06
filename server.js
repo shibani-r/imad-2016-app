775447cfe0d3d47f3e62acd818e87849a5672f93
shibani-r <shibani.v94@gmail.com>
2016-10-06 19:09:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -59,15 +59,8 @@ var htmlTemplate=`
         </title>
     <link href="/ui/style.css" rel="stylesheet" />
     </head>
-    <body><div id="mySidenav" class="sidenav">
-                <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-                <a href="/">Home</a>
-                <a href="/article-one">Article One</a>
-                <a href="/article-two">Article Two</a>
-                <a href="/article-three">Article Three</a>
-                </div>
+    <body>
     <div id="main">
-        <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; goto</span>
             
         <div class="container">
           
@@ -127,20 +120,7 @@ var htmlTemplate=`
                 request.send(null);
             };
     </script>
-    <script>
-            function openNav() {
-            document.getElementById("mySidenav").style.width = "250px";
-            document.getElementById("main").style.marginLeft = "250px";
-            document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-            }
-
-            function closeNav() {
-            document.getElementById("mySidenav").style.width = "0";
-            document.getElementById("main").style.marginLeft= "0";
-            document.body.style.backgroundColor = "white";
-            }
-    </script>
-   </body>
+       </body>
 </html>
 `;
 return htmlTemplate;
