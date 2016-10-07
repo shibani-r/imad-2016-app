75617d02f00afcf9ccc48a3290338eb0f946c2a5
shibani-r <shibani.v94@gmail.com>
2016-10-07 19:31:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -44,13 +44,11 @@ submit.onclick = function () {
          //capture a list of names and render it as a list
     var names = request.responseText;
     names = JSON.parse(names);
-    var comments = request.responseText;
-    comments = JSON.parse(comments);
-    var clist = '';
-    for(var i=0; i< names.length,i< comments.length; i++){
-        clist += '<p>' + names[i] + " : " + comments[i]'</p>';
+    var list = '';
+    for(var i=0; i< names.length; i++){
+        list += '<li>' + names[i] + '</li>';
     }
-    var div = document.getElementById('list');
+    var div = document.getElementById('namelist');
     div.innerHTML = list;
     }
     }
@@ -62,12 +60,42 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
     request.open('GET','http://shibani-r.imad.hasura-app.io/submit-name?name=' + name, true);
     request.send(null);
+};
+   
+  
+//submit comment
+
+var add = document.getElementById('add_btn');
+add.onclick = function () {
+    //make a request to the server and send the comment
     
-    //make the request
+     //create a request object
+    var request = new XMLHttpRequest();
+    
+    //capture the response and store it in a variable
+    request.onreadystatechange = function(){
+    // process the server response
+    if(request.readyState === XMLHttpRequest.DONE){
+        //take some action
+        if(request.status === 200){
+         //capture a list of comments and render it as a list
+    var comments = request.responseText;
+    comments = JSON.parse(comments);
+    var clist = '';
+    for(var j=0; j< comments.length; j++){
+        clist += '<li>' + comments[j] + '</li>';
+    }
+    var div = document.getElementById('commentlist');
+    div.innerHTML = clist;
+    }
+    }
+    //not done yet
+};
+
+//make the request
 var commentInput = document.getElementById('comment');
 var comment = commentInput.value;
     request.open('GET','http://shibani-r.imad.hasura-app.io/add-comment?comment=' + comment, true);
     request.send(null);
+
 };
-   
-  
