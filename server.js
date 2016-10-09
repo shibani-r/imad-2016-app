e01eab986758a6da8a77f6102ec202e8a8e48ae5
shibani-r <shibani.v94@gmail.com>
2016-10-09 13:28:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -104,13 +104,13 @@ var htmlTemplate=`
             function openNav() {
              document.getElementById("mySidenav").style.width = "250px";
              document.getElementById("main").style.marginLeft = "250px";
-            document.body.style.opacity = 0.4;
+            document.main.style.opacity = 0.25;
             }
 
             function closeNav() {
              document.getElementById("mySidenav").style.width = "0";
             document.getElementById("main").style.marginLeft= "0";
-             document.body.style.opacity = 1;
+             document.main.style.opacity = 1;
             }
         </script>
    <script>
