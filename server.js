f6e3c5379a1e5b35422eba86ca8222467606b0d4
shibani-r <shibani.v94@gmail.com>
2016-10-09 13:24:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -104,13 +104,13 @@ var htmlTemplate=`
             function openNav() {
              document.getElementById("mySidenav").style.width = "250px";
              document.getElementById("main").style.marginLeft = "250px";
-            document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+            document.body.style.background-color = "rgba(0,0,0,0.4)";
             }
 
             function closeNav() {
              document.getElementById("mySidenav").style.width = "0";
             document.getElementById("main").style.marginLeft= "0";
-             document.body.style.backgroundColor = "white";
+             document.body.style.background = URL("https://static.pexels.com/photos/54204/pexels-photo-54204.jpeg") no-repeat center center fixed; ;
             }
         </script>
    <script>
