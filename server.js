56b12efcd3e6eb67a15c120674530fc3823afa23
shibani-r <shibani.v94@gmail.com>
2016-10-25 14:24:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -112,40 +112,40 @@ var htmlTemplate=`
    <script>
         //submit comment
 
-            var add = document.getElementById('add_btn');
-            add.onclick = function () {
-            //make a request to the server and send the comment
+var add = document.getElementById('add_btn');
+add.onclick = function () {
+    //make a request to the server and send the comment
     
-            //create a request object
-            ar request = new XMLHttpRequest();
+     //create a request object
+    var request = new XMLHttpRequest();
     
-            //capture the response and store it in a variable
-            request.onreadystatechange = function(){
-                // process the server response
-                if(request.readyState === XMLHttpRequest.DONE){
-                    //take some action
-                    if(request.status === 200){
-                        //capture a list of comments and render it as a list
-                        var comments = request.responseText;
-                        comments = JSON.parse(comments);
-                        var clist = '';
-                        for(var j=0; j< comments.length; j++){
-                            clist += '<p>' + '"' + comments[j] + '"' + '</p>';
-                        }      
-                        var div = document.getElementById('commentlist');
-                        div.innerHTML = clist;
-                    }
-                }
-                //not done yet
-            };
+    //capture the response and store it in a variable
+    request.onreadystatechange = function(){
+    // process the server response
+    if(request.readyState === XMLHttpRequest.DONE){
+        //take some action
+        if(request.status === 200){
+         //capture a list of comments and render it as a list
+    var comments = request.responseText;
+    comments = JSON.parse(comments);
+    var clist = '';
+    for(var j=0; j< comments.length; j++){
+        clist += '<p>' + '"' + comments[j] + '"' + '</p>';
+    }
+    var div = document.getElementById('commentlist');
+    div.innerHTML = clist;
+    }
+    }
+    //not done yet
+};
 
-            //make the request
-            var commentInput = document.getElementById('comment');
-            var comment = commentInput.value;
-            request.open('GET','http://shibani-r.imad.hasura-app.io/add-comment?comment=' + comment, true);
-            request.send(null);
+//make the request
+var commentInput = document.getElementById('comment');
+var comment = commentInput.value;
+    request.open('GET','http://shibani-r.imad.hasura-app.io/add-comment?comment=' + comment, true);
+    request.send(null);
 
-        };
+};
 
     </script>
        </body>
