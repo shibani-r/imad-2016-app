2bf8cd4c7b0e6012686c057fb2e14063af6e2858
shibani-r <shibani.v94@gmail.com>
2016-10-25 12:59:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
 
+var config={
+user: 'shibani-r',
+database: 'shibani-r',
+host: 'db.imad.hasura-app.io',
+port: '5432',
+password: process.env.DB-PASSWORD
+};
 var app = express();
 app.use(morgan('combined'));
 var articles = {
@@ -154,7 +162,21 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-
+var Pool = new Pool(config);
+app.get('/test-db',function(req,res){
+//make a select request
+//return a response with the results
+Pool.query('SELECT * FROM test',function(err,res){
+    if(err)
+    {
+        res.status(500).send(err.toString());
+    }
+    else
+    {
+        res.send(JSON.stringify(result));
+    }
+});
+});
 
 var counter = 0;
 app.get('/counter',function(req, res){
