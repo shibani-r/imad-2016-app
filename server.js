57c4ea006226dd5e41b22765dcce66c6d7b399fa
shibani-r <shibani.v94@gmail.com>
2016-10-26 21:26:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config = {
     database : 'shibani-r',
     host : 'db.imad.hasura-app.io',
     port : '5432',
-    password : 'db-shibani-r-70402'
+    password : process.env.DB_PASSWORD
 };
 var app = express();
 app.use(morgan('combined'));
