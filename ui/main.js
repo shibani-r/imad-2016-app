ff6bed91a4adefcfcf01d89f710e49778a9cb657
shibani-r <shibani.v94@gmail.com>
2016-10-27 14:28:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,4 +28,39 @@ button.onclick = function (){
 
 
    
-  
+   //submit comment
+                        
+                                    var add = document.getElementById('add_btn');
+                                    add.onclick = function () {
+                                        //make a request to the server and send the comment
+                                        
+                                         //create a request object
+                                        var request = new XMLHttpRequest();
+                                        
+                                        //capture the response and store it in a variable
+                                        request.onreadystatechange = function(){
+                                        // process the server response
+                                        if(request.readyState === XMLHttpRequest.DONE){
+                                            //take some action
+                                            if(request.status === 200){
+                                             //capture a list of comments and render it as a list
+                                        var comments = request.responseText;
+                                        comments = JSON.parse(comments);
+                                        var clist = '';
+                                        for(var i=0; i< comments.length; i++){
+                                            clist += '<p>' + 'comment ' + i + ' : ' + '"' + comments[i] + '"' + '</p>';
+                                        }
+                                        var div = document.getElementById('commentlist');
+                                        div.innerHTML = clist;
+                                        }
+                                        }
+                                        //not done yet
+                                    };
+                                    
+                                    //make the request
+                                    var commentInput = document.getElementById('comment');
+                                    var comment = commentInput.value;
+                                        request.open('GET','http://shibani-r.imad.hasura-app.io/add-comment?comment=' + comment, true);
+                                        request.send(null);
+                                    
+                                    };
