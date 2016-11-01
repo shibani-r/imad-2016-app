5e51765fdc03699e445844b57cdb9f8a10621ad9
shibani-r <shibani.v94@gmail.com>
2016-11-01 20:41:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -42,16 +42,7 @@ function createTemplate(data){
                                 </div>
                         
                                 <div id="main">
-                                <div class="w3-top">
-  <ul class="w3-navbar w3-red w3-large">
-    <li><span onclick="openNav()"><i class="fa fa-bars"></i> goto</span></li>
-    <li><a href="#">Link 1</a></li>
-    <li><a href="#">Link 2</a></li>
-  </ul>
-</div>
-  
-  
-                                        
+                                    <span onclick="openNav()"><i class="fa fa-bars"></i> goto</span>
                                         <h1 class="article-heading">
                                         ${heading}
                                         </h1>
