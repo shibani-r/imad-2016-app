cc6c3d1c6f10c18c499d26b21702199e3e6438cf
shibani-r <shibani.v94@gmail.com>
2016-11-01 20:35:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ function createTemplate(data){
                                 </title>
                                 <link href="/ui/style.css" rel="stylesheet" />
                                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+                                <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
                                 <meta name="viewport" content="width=device-width, initial-scale=1">
                             </head>
                             
