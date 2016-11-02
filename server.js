e4af6d08bf08d2275072a0f1dd4c52f88e01bcbf
shibani-r <shibani.v94@gmail.com>
2016-11-02 17:09:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ function createTemplate(data){
    
     var htmlTemplate = `<!DOCTYPE html>
 <html>
-<title>W3.CSS Template</title>
+<title>${title}</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
@@ -36,9 +36,9 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
 
 <!-- Navbar -->
 <div class="w3-top">
-  <ul class="w3-navbar w3-red w3-card-2 w3-left-align w3-large">
+  <ul class="w3-navbar w3-black w3-card-2 w3-left-align w3-large">
     <li class="w3-hide-medium w3-hide-large w3-opennav w3-right">
-      <a class="w3-padding-large w3-hover-white w3-large w3-red" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+      <a class="w3-padding-large w3-hover-white w3-large w3-black" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
     </li>
     <li><a href="#" class="w3-padding-large w3-white">Home</a></li>
     <li class="w3-hide-small"><a href="#" class="w3-padding-large w3-hover-white">Link 1</a></li>
@@ -60,9 +60,8 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
 
 <!-- Header -->
 <header class="w3-container w3-red w3-center w3-padding-128">
-  <h1 class="w3-margin w3-jumbo">START PAGE</h1>
-  <p class="w3-xlarge">Template by w3.css</p>
-  <button class="w3-btn w3-padding-16 w3-large w3-margin-top">Get Started</button>
+  <h1 class="w3-margin w3-jumbo">${heading}</h1>
+  <p class="w3-xlarge"><i class="fa fa-clock-o"> </i> ${date.toDateString()}</p>
 </header>
 
 <!-- First Grid -->
