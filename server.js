4b98869a276a37292756900efb563b6dd752cd6a
shibani-r <shibani.v94@gmail.com>
2016-11-02 12:57:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -304,8 +304,7 @@ function createTemplate(data){
 
 </div>
 
-<!-- Add Google Maps -->
-<script src="https://maps.googleapis.com/maps/api/js"></script>
+
 
 <script>
                                     function openNav() {
@@ -319,29 +318,7 @@ function createTemplate(data){
                                     }
                                 </script>
                                 
-<script>
-var myCenter = new google.maps.LatLng(41.878114, -87.629798);
 
-function initialize() {
-var mapProp = {
-  center:myCenter,
-  zoom:12,
-  scrollwheel:false,
-  draggable:false,
-  mapTypeId:google.maps.MapTypeId.ROADMAP
-  };
-
-var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-
-var marker = new google.maps.Marker({
-  position:myCenter,
-  });
-
-marker.setMap(map);
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
-</script>
 
 <footer class="container-fluid text-center">
   <a href="#myPage" title="To Top">
