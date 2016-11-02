0b5fb09ffcd0a538dc0accb25a0283b1b0f4508d
shibani-r <shibani.v94@gmail.com>
2016-11-02 17:51:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -46,9 +46,9 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
 
 <!-- Navbar -->
 <div class="w3-top">
-  <ul class="w3-navbar w3-black w3-card-2 w3-left-align w3-large">
+  <ul class="w3-navbar w3-red w3-card-2 w3-left-align w3-large">
     <li class="w3-hide-medium w3-hide-large w3-opennav w3-right">
-      <a class="w3-padding-large w3-hover-white w3-large w3-black" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+      <a class="w3-padding-large w3-hover-white w3-large w3-red" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
     </li>
     <li><a href="#" class="w3-padding-large w3-white">Home</a></li>
     <li class="w3-hide-small"><a href="#article-content" class="w3-padding-large w3-hover-white">Content</a></li>
