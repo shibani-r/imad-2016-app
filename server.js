7244472fa83aea6ac3a2ac42796493c00ae5b2da
shibani-r <shibani.v94@gmail.com>
2016-11-03 14:35:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -46,13 +46,23 @@ function createTemplate(data){
             </head>
             <body>
                      
+                    <div id="myNav" class="overlay">
+                        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+                        <div class="overlay-content">
+                            <a href="#">About</a>
+                            <a href="#">Services</a>
+                            <a href="#">Clients</a>
+                            <a href="#">Contact</a>
+                        </div>
+                    </div>
+                    
                 <!-- Navbar -->
                 <div class="w3-top">
                     <ul class="w3-navbar w3-black w3-card-2 w3-left-align w3-large">
                         <li class="w3-hide-medium w3-hide-large w3-opennav w3-right">
                           <a class="w3-padding-large w3-hover-white w3-large w3-red" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
                         </li>
-                        <li><a href="/" class="w3-padding-large w3-white">Home</a></li>
+                        <li><a href="#" class="w3-padding-large w3-white"><span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; open</span></a></li>
                         <li class="w3-hide-small"><a href="#article-content" class="w3-padding-large w3-hover-white">Content</a></li>
                         <li class="w3-hide-small"><a href="#comments-section" class="w3-padding-large w3-hover-white">Comments</a></li>
                         <li class="w3-hide-small"><a href="#" class="w3-padding-large w3-hover-white">Link 3</a></li>
@@ -146,6 +156,16 @@ function createTemplate(data){
                         } else {
                             x.className = x.className.replace(" w3-show", "");
                         }
+                    }
+                </script>
+                
+                <script>
+                    function openNav() {
+                        document.getElementById("myNav").style.width = "100%";
+                    }
+                    
+                    function closeNav() {
+                        document.getElementById("myNav").style.width = "0%";
                     }
                 </script>
     
