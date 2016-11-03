25eaea0928fe03186f4f4ca1d2bc0103dad9dcd2
shibani-r <shibani.v94@gmail.com>
2016-11-03 12:53:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,164 +20,163 @@ function createTemplate(data){
     var content = data.content;
    
     var htmlTemplate = `<!DOCTYPE html>
-<html>
-<title>${title}</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
- <link href="/ui/style.css" rel="stylesheet" />
-<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
-<style>
-body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
-.w3-navbar,h1,button {font-family: "Montserrat", sans-serif}
-.fa-music,.fa-comments-o {font-size:200px}
-header {background: URL("http://www.intrawallpaper.com/static/images/Backgrounds-HD-3.jpg") no-repeat center center fixed;}
-</style>
-<body>
-                 
-                        
-<!-- Navbar -->
-<div class="w3-top">
-  <ul class="w3-navbar w3-black w3-card-2 w3-left-align w3-large">
-    <li class="w3-hide-medium w3-hide-large w3-opennav w3-right">
-      <a class="w3-padding-large w3-hover-white w3-large w3-red" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
-    </li>
-    <li><a href="/" class="w3-padding-large w3-white">Home</a></li>
-    <li class="w3-hide-small"><a href="#article-content" class="w3-padding-large w3-hover-white">Content</a></li>
-    <li class="w3-hide-small"><a href="#comments-section" class="w3-padding-large w3-hover-white">Comments</a></li>
-    <li class="w3-hide-small"><a href="#" class="w3-padding-large w3-hover-white">Link 3</a></li>
-    <li class="w3-hide-small"><a href="#" class="w3-padding-large w3-hover-white">Link 4</a></li>
-  </ul>
-
-  <!-- Navbar on small screens -->
-  <div id="navDemo" class="w3-hide w3-hide-large w3-hide-medium">
-    <ul class="w3-navbar w3-left-align w3-large w3-black">
-      <li><a class="w3-padding-large" href="#article-content">Content</a></li>
-      <li><a class="w3-padding-large" href="#comments-section">Comments</a></li>
-      <li><a class="w3-padding-large" href="#">Link 3</a></li>
-      <li><a class="w3-padding-large" href="#">Link 4</a></li>
-    </ul>
-  </div>
-</div>
-
-<!-- Header -->
-<header class="w3-container w3-center w3-padding-128">
-  <h1 class="w3-margin w3-jumbo">${heading}</h1>
-  <p class="w3-xlarge"><i class="fa fa-clock-o"> </i> ${date.toDateString()}</p>
-  
-</header>
-
-<!-- First Grid -->
-<div class="w3-row-padding w3-padding-64 w3-container" id="article-content">
-  <div class="w3-content">
-    <div class="w3-twothird">
-      <h1>${heading}</h1>
-      <h5 class="w3-padding-32">${content}</h5>
-
-      <p class="w3-text-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat.</p>
+    <title>${title}</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+     <link href="/ui/style.css" rel="stylesheet" />
+    <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+    <style>
+    body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
+    .w3-navbar,h1,button {font-family: "Montserrat", sans-serif}
+    .fa-music,.fa-comments-o {font-size:200px}
+    header {background: URL("http://www.intrawallpaper.com/static/images/Backgrounds-HD-3.jpg") no-repeat center center fixed;}
+    </style>
+    <body>
+                     
+                            
+    <!-- Navbar -->
+    <div class="w3-top">
+      <ul class="w3-navbar w3-black w3-card-2 w3-left-align w3-large">
+        <li class="w3-hide-medium w3-hide-large w3-opennav w3-right">
+          <a class="w3-padding-large w3-hover-white w3-large w3-red" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+        </li>
+        <li><a href="/" class="w3-padding-large w3-white">Home</a></li>
+        <li class="w3-hide-small"><a href="#article-content" class="w3-padding-large w3-hover-white">Content</a></li>
+        <li class="w3-hide-small"><a href="#comments-section" class="w3-padding-large w3-hover-white">Comments</a></li>
+        <li class="w3-hide-small"><a href="#" class="w3-padding-large w3-hover-white">Link 3</a></li>
+        <li class="w3-hide-small"><a href="#" class="w3-padding-large w3-hover-white">Link 4</a></li>
+      </ul>
+    
+      <!-- Navbar on small screens -->
+      <div id="navDemo" class="w3-hide w3-hide-large w3-hide-medium">
+        <ul class="w3-navbar w3-left-align w3-large w3-black">
+          <li><a class="w3-padding-large" href="#article-content">Content</a></li>
+          <li><a class="w3-padding-large" href="#comments-section">Comments</a></li>
+          <li><a class="w3-padding-large" href="#">Link 3</a></li>
+          <li><a class="w3-padding-large" href="#">Link 4</a></li>
+        </ul>
+      </div>
     </div>
-
-    <div class="w3-third w3-center">
-      <i class="fa fa-music w3-padding-64 w3-text-red "></i>
+    
+    <!-- Header -->
+    <header class="w3-container w3-center w3-padding-128">
+      <h1 class="w3-margin w3-jumbo">${heading}</h1>
+      <p class="w3-xlarge"><i class="fa fa-clock-o"> </i> ${date.toDateString()}</p>
+      
+    </header>
+    
+    <!-- First Grid -->
+    <div class="w3-row-padding w3-padding-64 w3-container" id="article-content">
+      <div class="w3-content">
+        <div class="w3-twothird">
+          <h1>${heading}</h1>
+          <h5 class="w3-padding-32">${content}</h5>
+    
+          <p class="w3-text-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint
+            occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+            laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+    
+        <div class="w3-third w3-center">
+          <i class="fa fa-music w3-padding-64 w3-text-red "></i>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-
-<!-- Second Grid -->
-<div class="w3-row-padding w3-black w3-padding-64 w3-container" id="comments-section">
-  <div class="w3-content">
-    <div class="w3-third w3-center">
-      <i class="fa fa-comments-o w3-padding-64 w3-text-red w3-margin-right"></i>
+    
+    <!-- Second Grid -->
+    <div class="w3-row-padding w3-black w3-padding-64 w3-container" id="comments-section">
+      <div class="w3-content">
+        <div class="w3-third w3-center">
+          <i class="fa fa-comments-o w3-padding-64 w3-text-red w3-margin-right"></i>
+        </div>
+    
+        <div class="w3-twothird">
+          <h1>Comments</h1>
+          <h5 class="w3-padding-32"><i class="fa fa-pencil"></i> post your comments here</h5>
+    
+          <p class="w3-text-black"><form><input type="text" id="comment" palceholder="comment"></input></form>
+                                                <input type="submit" value="post" id="add_btn"></input>
+                                                <div id="commentlist">
+                                                </div></p>
+        </div>
+      </div>
     </div>
-
-    <div class="w3-twothird">
-      <h1>Comments</h1>
-      <h5 class="w3-padding-32"><i class="fa fa-pencil"></i> post your comments here</h5>
-
-      <p class="w3-text-black"><form><input type="text" id="comment" palceholder="comment"></input></form>
-                                            <input type="submit" value="post" id="add_btn"></input>
-                                            <div id="commentlist">
-                                            </div></p>
+    
+    <div class="w3-container w3-black w3-center w3-opacity w3-padding-64">
+        <h1 class="w3-margin w3-xlarge">Quote of the day: live life</h1>
     </div>
-  </div>
-</div>
-
-<div class="w3-container w3-black w3-center w3-opacity w3-padding-64">
-    <h1 class="w3-margin w3-xlarge">Quote of the day: live life</h1>
-</div>
-
-<!-- Footer -->
-<footer class="w3-container w3-padding-64 w3-center w3-opacity">
-  <div class="w3-xlarge w3-padding-32">
-   <a href="#" class="w3-hover-text-indigo"><i class="fa fa-facebook-official"></i></a>
-   <a href="#" class="w3-hover-text-red"><i class="fa fa-pinterest-p"></i></a>
-   <a href="#" class="w3-hover-text-light-blue"><i class="fa fa-twitter"></i></a>
-   <a href="#" class="w3-hover-text-grey"><i class="fa fa-flickr"></i></a>
-   <a href="#" class="w3-hover-text-indigo"><i class="fa fa-linkedin"></i></a>
- </div>
-</footer>
-
-</div>
-
-<script>
-// Used to toggle the menu on small screens when clicking on the menu button
-function myFunction() {
-    var x = document.getElementById("navDemo");
-    if (x.className.indexOf("w3-show") == -1) {
-        x.className += " w3-show";
-    } else {
-        x.className = x.className.replace(" w3-show", "");
+    
+    <!-- Footer -->
+    <footer class="w3-container w3-padding-64 w3-center w3-opacity">
+      <div class="w3-xlarge w3-padding-32">
+       <a href="#" class="w3-hover-text-indigo"><i class="fa fa-facebook-official"></i></a>
+       <a href="#" class="w3-hover-text-red"><i class="fa fa-pinterest-p"></i></a>
+       <a href="#" class="w3-hover-text-light-blue"><i class="fa fa-twitter"></i></a>
+       <a href="#" class="w3-hover-text-grey"><i class="fa fa-flickr"></i></a>
+       <a href="#" class="w3-hover-text-indigo"><i class="fa fa-linkedin"></i></a>
+     </div>
+    </footer>
+    
+    </div>
+    
+    <script>
+    // Used to toggle the menu on small screens when clicking on the menu button
+    function myFunction() {
+        var x = document.getElementById("navDemo");
+        if (x.className.indexOf("w3-show") == -1) {
+            x.className += " w3-show";
+        } else {
+            x.className = x.className.replace(" w3-show", "");
+        }
     }
-}
-</script>
-
-<script>
-                                //submit comment
-                        
-                                    var add = document.getElementById('add_btn');
-                                    add.onclick = function () {
-                                        //make a request to the server and send the comment
+    </script>
+    
+    <script>
+                                    //submit comment
+                            
+                                        var add = document.getElementById('add_btn');
+                                        add.onclick = function () {
+                                            //make a request to the server and send the comment
+                                            
+                                             //create a request object
+                                            var request = new XMLHttpRequest();
+                                            
+                                            //capture the response and store it in a variable
+                                            request.onreadystatechange = function(){
+                                            // process the server response
+                                            if(request.readyState === XMLHttpRequest.DONE){
+                                                //take some action
+                                                if(request.status === 200){
+                                                 //capture a list of comments and render it as a list
+                                            var comments = request.responseText;
+                                            comments = JSON.parse(comments);
+                                            var clist = '';
+                                            for(var i=0; i< comments.length; i++){
+                                                clist += '<p>' + '<i class="fa fa-comment-o"></i>' + ' comment ' + i + ' : ' + '"' + comments[i] + '"' + '</p>';
+                                            }
+                                            var div = document.getElementById('commentlist');
+                                            div.innerHTML = clist;
+                                            }
+                                            }
+                                            //not done yet
+                                        };
                                         
-                                         //create a request object
-                                        var request = new XMLHttpRequest();
+                                        //make the request
+                                        var commentInput = document.getElementById('comment');
+                                        var comment = commentInput.value;
+                                            request.open('GET','http://shibani-r.imad.hasura-app.io/add-comment?comment=' + comment, true);
+                                            request.send(null);
                                         
-                                        //capture the response and store it in a variable
-                                        request.onreadystatechange = function(){
-                                        // process the server response
-                                        if(request.readyState === XMLHttpRequest.DONE){
-                                            //take some action
-                                            if(request.status === 200){
-                                             //capture a list of comments and render it as a list
-                                        var comments = request.responseText;
-                                        comments = JSON.parse(comments);
-                                        var clist = '';
-                                        for(var i=0; i< comments.length; i++){
-                                            clist += '<p>' + '<i class="fa fa-comment-o"></i>' + ' comment ' + i + ' : ' + '"' + comments[i] + '"' + '</p>';
-                                        }
-                                        var div = document.getElementById('commentlist');
-                                        div.innerHTML = clist;
-                                        }
-                                        }
-                                        //not done yet
-                                    };
-                                    
-                                    //make the request
-                                    var commentInput = document.getElementById('comment');
-                                    var comment = commentInput.value;
-                                        request.open('GET','http://shibani-r.imad.hasura-app.io/add-comment?comment=' + comment, true);
-                                        request.send(null);
-                                    
-                                    };
-
-                                    
-                                </script>
-
-</body>
-</html>
-
+                                        };
+    
+                                        
+                                    </script>
+    
+    </body>
+    </html>
+    
 
 
 `;
