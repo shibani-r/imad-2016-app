99af463e240739c2d9cd19feec2a7499645c7946
shibani-r <shibani.v94@gmail.com>
2016-11-03 15:04:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -41,8 +41,7 @@ function createTemplate(data){
                                 -o-background-size: cover;       
                                 background-size: cover; 
                                 color:white;
-                    #quote_box {background: URL("http://www.intrawallpaper.com/static/images/Backgrounds-HD-3.jpg") no-repeat center center fixed;}            
-                    }
+                   
                 </style>
             </head>
             <body>
@@ -130,7 +129,7 @@ function createTemplate(data){
                 </div>
                 
                 <!--Quote of the day-->
-                <div  id="quote_box" class="w3-container w3-center w3-opacity w3-padding-64">
+                <div  id="quote_box" class="w3-container w3-red w3-center w3-opacity w3-padding-64">
                     <h1 class="w3-margin w3-xlarge">Quote of the day: live life</h1>
                 </div>
                 
