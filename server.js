db72aa9681a22885c2e5a22223441ed3287423ee
shibani-r <shibani.v94@gmail.com>
2016-11-03 13:24:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ function createTemplate(data){
                     <div class="w3-content">
                         <div class="w3-twothird">
                             <h1>${heading}</h1>
-                            <h5 class="w3-padding-32">${content}</h5>
+                            <h5 class="w3-padding-32 w3-justify">${content}</h5>
                 
                             <p class="w3-text-grey">
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint
