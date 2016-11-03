7f7ef2b0dfad0984b573e00b9c1f74ed7895d20b
shibani-r <shibani.v94@gmail.com>
2016-11-03 15:06:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -131,7 +131,7 @@ function createTemplate(data){
                 </div>
                 
                 <!--Quote of the day-->
-                <div  id="quote_box" class="w3-container w3-red w3-center w3-opacity w3-padding-64">
+                <div  id="quote_box" class="w3-container w3-center w3-padding-64">
                     <h1 class="w3-margin w3-xlarge">Quote of the day: live life</h1>
                 </div>
                 
