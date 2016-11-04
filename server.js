bb936029e5f0ccb1d02fbefa412710001a50df17
shibani-r <shibani.v94@gmail.com>
2016-11-04 15:19:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -194,7 +194,7 @@ function createTemplate(data){
                                     comments = JSON.parse(comments);
                                     var clist = '';
                                     for(var i=0; i< comments.length; i++) {
-                                        clist += '<p>' + '<i class="fa fa-comment-o"></i>' + ' comment ' + i + ' : ' + '"' + comments[i] + '"' + '</p>';
+                                        clist += '<pre>' + '<i class="fa fa-comment-o"></i>' + ' comment ' + i + ' : ' + '"' + comments[i] + '"' + '</pre>';
                                     }
                                     
                                     var div = document.getElementById('commentlist');
