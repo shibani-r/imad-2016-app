4582d51f439b0ef8509303a8b1166f5ee6eb42d3
shibani-r <shibani.v94@gmail.com>
2016-11-05 16:36:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -242,6 +242,7 @@ app.get('/hash/:input', function (req,res) {
 
 app.post('/create-user', function (req,res) {
     //username, password
+    //{username': "shibani" , 'password' : "password"}
     //JSON
     var username = req.body.username;
     var password = req.body.password;
