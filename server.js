428e74def960b4544a00dd0422aa3ba0637a8e44
shibani-r <shibani.v94@gmail.com>
2016-11-06 17:04:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -116,7 +116,7 @@ function createTemplate(data){
                 </div>
                 
                 <!-- Second Grid -->
-                <div class="w3-row-padding w3-red w3-padding-64 w3-container" id="comments-section">
+                <div class="w3-row-padding w3-black w3-padding-64 w3-container" id="comments-section">
                     <div class="w3-content">
                         <div class="w3-third w3-center">
                             <i class="fa fa-comments-o w3-padding-64 w3-text-orange "></i>
@@ -126,13 +126,13 @@ function createTemplate(data){
                             <h1>Comments</h1>
                             <h5 class="w3-padding-32"><i class="fa fa-pencil"></i> post your comments here</h5>
                 
-                            <p class="w3-text-red">
-                                <div id="comment_form">
-              </div>
-              <div id="comments">
-                <center>Loading comments...</center>
-              </div>
-                            
+                            <p class="w3-text-black">
+                                <form>  
+                                    <textarea id="comment" palceholder="comment"></textarea>
+                                </form>
+                                <input type="submit" value="post" id="add_btn"></input>
+                                <div id="commentlist">
+                                </div>
                             </p>
                         </div>
                         
@@ -178,8 +178,50 @@ function createTemplate(data){
                     }
                 </script>
     
-                <script type="text/javascript" src="/ui/article.js"></script>
-
+                <script>
+                //submit comment
+                            
+                    var add = document.getElementById('add_btn');
+                    add.onclick = function () {
+                    //make a request to the server and send the comment
+                                            
+                        //create a request object
+                        var request = new XMLHttpRequest();
+                                            
+                        //capture the response and store it in a variable
+                        request.onreadystatechange = function(){
+                        
+                            // process the server response
+                            if(request.readyState === XMLHttpRequest.DONE) {
+                            
+                                //take some action
+                                if(request.status === 200) {
+                                
+                                    //capture a list of comments and render it as a list
+                                    var comments = request.responseText;
+                                    comments = JSON.parse(comments);
+                                    var clist = '';
+                                    for(var i=0; i< comments.length; i++) {
+                                        clist += '<p id="comment_box">'+ '<i class="fa fa-comment-o"></i>' + ' comment ' + i + ' : ' + '"' + comments[i] + '"' + '</p>';
+                                    }
+                                    
+                                    var div = document.getElementById('commentlist');
+                                    div.innerHTML = clist;
+                                }
+                            }
+                            //not done yet
+                        };
+                                        
+                        //make the request
+                        var commentInput = document.getElementById('comment');
+                        var comment = commentInput.value;
+                        request.open('GET','http://shibani-r.imad.hasura-app.io/add-comment?comment=' + comment, true);
+                        request.send(null);
+                                        
+                    };
+    
+                                        
+                </script>
     
             </body>
         </html>
@@ -256,48 +298,28 @@ app.post('/login', function (req, res) {
 
 app.get('/check-login', function (req, res) {
     if (req.session && req.session.auth && req.session.auth.userId) {
-       // Load the user object
-       pool.query('SELECT * FROM "user" WHERE id = $1', [req.session.auth.userId], function (err, result) {
-           if (err) {
-              res.status(500).send(err.toString());
-           } else {
-              res.send(result.rows[0].username);    
-           }
-       });
-   } else {
-       res.status(400).send('You are not logged in');
-   }
+        res.send('you are logged in: ' + req.session.auth.userId.toString());
+    } else {
+        res.send('you are not logged in');
+    }
 });
 
 app.get('/logout', function (req, res) {
     delete req.session.auth;
-    res.send('<html><body>Logged out!<br/><br/><a href="/">Back to home</a></body></html>');
+    res.send('logged out');
 });
 
 var pool = new Pool(config);
-
-app.get('/get-articles', function (req, res) {
-   // make a select request
-   // return a response with the results
-   pool.query('SELECT * FROM article ORDER BY date DESC', function (err, result) {
-      if (err) {
-          res.status(500).send(err.toString());
-      } else {
-          res.send(JSON.stringify(result.rows));
-      }
-   });
-});
-
-app.get('/get-comments/:articleName', function (req, res) {
-   // make a select request
-   // return a response with the results
-   pool.query('SELECT comment.*, "user".username FROM article, comment, "user" WHERE article.title = $1 AND article.id = comment.article_id AND comment.user_id = "user".id ORDER BY comment.timestamp DESC', [req.params.articleName], function (err, result) {
-      if (err) {
-          res.status(500).send(err.toString());
-      } else {
-          res.send(JSON.stringify(result.rows));
-      }
-   });
+app.get('/test-db', function (req, res) {
+    // make a select request
+    // return a response with the results
+    pool.query('SELECT * FROM test', function (err, result) {
+        if(err) {
+            res.status(500).send(err.toString());
+        }   else {
+                res.send(JSON.stringify(result.rows));
+            }
+    });
 });
 
 var counter = 0;
@@ -306,37 +328,15 @@ app.get('/counter', function (req, res) {
     res.send(counter.toString());
 });
 
-app.post('/submit-comment/:articleName', function (req, res) {
-   // Check if the user is logged in
-    if (req.session && req.session.auth && req.session.auth.userId) {
-        // First check if the article exists and get the article-id
-        pool.query('SELECT * from article where title = $1', [req.params.articleName], function (err, result) {
-            if (err) {
-                res.status(500).send(err.toString());
-            } else {
-                if (result.rows.length === 0) {
-                    res.status(400).send('Article not found');
-                } else {
-                    var articleId = result.rows[0].id;
-                    // Now insert the right comment for this article
-                    pool.query(
-                        "INSERT INTO comment (comment, article_id, user_id) VALUES ($1, $2, $3)",
-                        [req.body.comment, articleId, req.session.auth.userId],
-                        function (err, result) {
-                            if (err) {
-                                res.status(500).send(err.toString());
-                            } else {
-                                res.status(200).send('Comment inserted!')
-                            }
-                        });
-                }
-            }
-       });     
-    } else {
-        res.status(403).send('Only logged in users can comment');
-    }
+var comments = [];
+app.get('/add-comment', function (req, res) {// /add-comment?comment=xxxx
+    // get the comment from the request
+    var comment = req.query.comment;
+    
+    comments.push(comment);
+    // JSON: javascript object notation
+    res.send(JSON.stringify(comments));
 });
-
 
 
 app.get('/articles/:articleName', function (req, res) {
@@ -358,10 +358,17 @@ app.get('/articles/:articleName', function (req, res) {
     });
 });
 
-app.get('/ui/:fileName', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', req.params.fileName));
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
