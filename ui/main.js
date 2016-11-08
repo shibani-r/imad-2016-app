291222a054b735a1520ae3d1e7da1b1e8deda3e8
shibani-r <shibani.v94@gmail.com>
2016-11-08 14:57:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,28 +32,31 @@ var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
     // make a request to the server and send the name
     
-     // create a request object
+    // create a request object
     var request = new XMLHttpRequest();
     
     // capture the response and store it in a variable
     request.onreadystatechange = function(){
-    // process the server response
-    if(request.readyState === XMLHttpRequest.DONE){
-        // take some action
-        if(request.status === 200){
-         // capture a list of names and render it as a list
-    console.log("user loggedin");
-        alert('logged in successfully');
-            
-        } else if(request.status ===403) {
-            alert("username/password is incorrect");
-    } else if(request.status === 500){
-        alert("something went wrong on the server");
+    
+        // process the server response
+        if(request.readyState === XMLHttpRequest.DONE){
         
-    }
-    }
+            // take some action
+            if(request.status === 200){
+            
+                // capture a list of names and render it as a list
+                console.log("user loggedin");
+                alert('logged in successfully');
+            
+            } else if(request.status ===403) {
+                    alert("username/password is incorrect");
+            } else if(request.status === 500) {
+                    alert("something went wrong on the server");
+        
+                }
+        }
     // not done yet
-};
+    };
 
 // make the request
 var username = document.getElementById('username').value;
