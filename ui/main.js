cac187cc22e2caa59055df18b62940139ca71d14
shibani-r <shibani.v94@gmail.com>
2016-11-08 15:01:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,21 @@
 
 // counter code
+
 var button = document.getElementById('counter');
 
-button.onclick = function (){
+button.onclick = function () {
     
     // create a request object
     var request = new XMLHttpRequest();
     
     // capture the response and store it in a variable
-    request.onreadystatechange = function(){
+    request.onreadystatechange = function() {
+        
         // process the server response
-        if(request.readyState === XMLHttpRequest.DONE){
+        if(request.readyState === XMLHttpRequest.DONE) {
+            
             // take some action
-            if(request.status === 200){
+            if(request.status === 200) {
                 var counter = request.responseText;
                 var span = document.getElementById('count');
                 span.innerHTML = counter.toString();
@@ -21,7 +24,7 @@ button.onclick = function (){
         // not done yet
     };
 
-// make the request
+    // make the request
     request.open('GET','http://shibani-r.imad.hasura-app.io/counter', true);
     request.send(null);
 };
@@ -36,33 +39,33 @@ submit.onclick = function () {
     var request = new XMLHttpRequest();
     
     // capture the response and store it in a variable
-    request.onreadystatechange = function(){
+    request.onreadystatechange = function() {
     
         // process the server response
-        if(request.readyState === XMLHttpRequest.DONE){
+        if(request.readyState === XMLHttpRequest.DONE) {
         
             // take some action
-            if(request.status === 200){
+            if(request.status === 200) {
             
                 // capture a list of names and render it as a list
                 console.log("user loggedin");
                 alert('logged in successfully');
             
-            } else if(request.status ===403) {
+            }   else if(request.status ===403) {
                     alert("username/password is incorrect");
-            } else if(request.status === 500) {
-                    alert("something went wrong on the server");
+                }   else if(request.status === 500) {
+                        alert("something went wrong on the server");
         
-                }
+                    }
         }
     // not done yet
     };
 
-// make the request
-var username = document.getElementById('username').value;
-var password = document.getElementById('password').value;
-console.log(username);
-console.log(password);
+    // make the request
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
     request.open('POST','http://shibani-r.imad.hasura-app.io/login', true);
     request.setRequestHeader('Content-Type', 'application/json');
     request.send(JSON.stringify({username: username, password: password}));
