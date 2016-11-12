8e1fd64577edbc03f6aba93ca7afcfa1ccf1ee1f
shibani-r <shibani.v94@gmail.com>
2016-11-12 18:03:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -36,7 +36,6 @@ function createTemplate(data){
             <head>
                 <title>${title}</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1">
-                <link rel="stylesheet" href="/ui/style.css">
                 <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
                 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
                 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
