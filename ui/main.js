1335fe0c406227233ef1fc5020d4d82029c86742
shibani-r <shibani.v94@gmail.com>
2016-11-12 15:30:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -71,5 +71,46 @@ submit.onclick = function () {
     request.send(JSON.stringify({username: username, password: password}));
 };
    
+   // submit username/password to login 
+
+var submitR = document.getElementById('submit_btnR');
+submitR.onclick = function () {
+    // make a request to the server and send the name
+    
+    // create a request object
+    var request = new XMLHttpRequest();
+    
+    // capture the response and store it in a variable
+    request.onreadystatechange = function() {
+    
+        // process the server response
+        if(request.readyState === XMLHttpRequest.DONE) {
+        
+            // take some action
+            if(request.status === 200) {
+            
+                // capture a list of names and render it as a list
+                console.log("user registered");
+                alert('registered in successfully');
+            
+            }   else if(request.status ===403) {
+                    alert("username/password is incorrect");
+                }   else if(request.status === 500) {
+                        alert("something went wrong on the server");
+        
+                    }
+        }
+    // not done yet
+    };
+
+    // make the request
+    var usernameR = document.getElementById('usernameR').value;
+    var passwordR = document.getElementById('passwordR').value;
+    console.log(usernameR);
+    console.log(passwordR);
+    request.open('POST','http://shibani-r.imad.hasura-app.io/create-user', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({username: usernameR, password: passwordR}));
+};
    
    
