e1c087e8de488668269924fec158d58907ae2ea5
shibani-r <shibani.v94@gmail.com>
2016-11-18 18:02:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -142,3 +142,22 @@ submitR.onclick = function () {
 };
    
    
+function loadLogin () {
+    // Check if the user is already logged in
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                loadLoggedInUser(this.responseText);
+            } else {
+                loadLoginForm();
+            }
+        }
+    };
+    
+    request.open('GET', '/check-login', true);
+    request.send(null);
+}
+
+// The first thing to do is to check if the user is logged in!
+loadLogin()
