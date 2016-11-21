7f6ef071f141d83bafa3f7a824f171663774f3ae
shibani-r <shibani.v94@gmail.com>
2016-11-21 15:43:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,7 +31,7 @@ function loadLoginForm () {
               // Take some action
               if (request.status === 200) {
                   submit.value = 'Success!';
-                  bootbox.alert('You are successfully logged in!');
+                  alert('You are successfully logged in!');
                   console.log('You are successfully logged in!');
               } else if (request.status === 403) {
                   submit.value = 'Invalid credentials. Try again?';
