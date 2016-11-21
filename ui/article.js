d35236ff2a034294982e52ed333e99553c17cad8
shibani-r <shibani.v94@gmail.com>
2016-11-21 18:47:45 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -75,12 +75,14 @@ function loadComments () {
                 var commentsData = JSON.parse(this.responseText);
                 for (var i=0; i< commentsData.length; i++) {
                     var time = new Date(commentsData[i].timestamp);
-                    content += `<div class="comment w3-panel w3-card-8 w3-white">
-                        <p><i>"${escapeHTML(commentsData[i].comment)}"</i></p>
-                        <div class="commenter">
-                            ${commentsData[i].username} - ${time.toLocaleTimeString()} on ${time.toLocaleDateString()} 
-                        </div>
-                    </div>`;
+                    content += `<div class="class="w3-panel w3-card-8 w3-black" style="padding:10px">
+                                   <div class="comment">
+                                        <p><i>"${escapeHTML(commentsData[i].comment)}"</i></p>
+                                        <div class="commenter">
+                                            ${commentsData[i].username} - ${time.toLocaleTimeString()} on ${time.toLocaleDateString()} 
+                                        </div>
+                                    </div>
+                                </div>`
                 }
                 comments.innerHTML = content;
             } else {
