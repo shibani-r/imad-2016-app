e7d6f9adbecbe937130f6bb70b4f6ff521555994
shibani-r <shibani.v94@gmail.com>
2016-11-24 14:32:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,9 +19,7 @@ app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(session({
     secret: 'someRandomSecretValue',
-    cookie: { maxAge: 1000 * 60 * 60 * 24 * 30},
-    resave: true,
-    saveUninitialized: true
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 30}
 }));
 
 function createTemplate (data){
