83ca90844c69149771f43b8fcfd068f484283a0e
shibani-r <shibani.v94@gmail.com>
2016-11-25 14:14:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -278,7 +278,7 @@ app.get('/check-login', function (req, res) {
 
 app.get('/logout', function (req, res) {
     delete req.session.auth;
-    res.send('You are logged out!');
+    res.send('<html><body>You are Logged Out!<br/><br/><a href="/">Back to home</a></body></html>');
     
 });
 
